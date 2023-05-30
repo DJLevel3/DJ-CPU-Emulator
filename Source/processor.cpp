fa3247c74f2007857d5d0dabbc3e15ea62b09e78
DJLevel3 <zachcommercialmail@gmail.com>
2023-05-30 11:22:22 -0600

    Update ISA and a few other things to match FPGA implementation (!!)

--- Source/processor.cpp
+++ Source/processor.cpp
@@ -28,110 +28,98 @@ void Processor::run(bool clock) {
 
     switch (processorState) {
     case 0:
-        if (clock) {
-            busAddressLatch = programCounter; // PA
-            readRequest = true;
-        }
-        else {
-            instructionRegister = busInputRegister; // IL
-        }
-        break;
+        busAddressLatch = programCounter; // PA
+        readRequest = true;
     case 1:
-        if (clock) {
-            programCounter++; // PI
-        }
-        else {
-            busAddressLatch = busAddressRegister;
-        }
+        instructionRegister = busInputRegister; // IL
         break;
     case 2:
-        if (clock) {
-            // rising edge
-            if (microcode1 & 2) {
-                busAddressLatch = busAddressRegister;
-                readRequest = true;
-                busOut = true;
-            }
-            if (microcode1 & 8)    addrOut = true;
-            if (microcode1 & 512)  accOut = true;
-            if (microcode1 & 2048) regOut = true;
-            if (microcode1 & 8192) opLow = true;
-        }
-        else {
-            // falling edge
-            if (microcode1 & 1)    busIn = true;
-            if (microcode1 & 4)    mathIn = true;
-            if (microcode1 & 16)   accInLow = true;
-            if (microcode1 & 32)   pcIn = true;
-            if (microcode1 & 64)   arLowIn = true;
-            if (microcode1 & 128)  arIn = true;
-            if (microcode1 & 256)  accIn = true;
-            if (microcode1 & 1024) regIn = true;
-            if (microcode1 & 4096) addrHigh = true;
-        }
-        break;
+        programCounter++; // PI
     case 3:
-        if (clock) {
-            // rising edge
-            if (microcode2 & 2) { carryFlag = false; zeroFlag = false; }
-            if (microcode2 & 16) interrupt = false;
-            if (microcode2 & 256) cRollLine = true;
-            if (microcode2 & 512) cInLine = true;
-            if (microcode2 & 1024) rightShift = true;
-            if (microcode2 & 2048) leftShift = true;
-            if (microcode2 & 4096) invBLine = true;
-            if (microcode2 & 8192) invALine = true;
-            if (microcode2 & 16384) fcLine = true;
-            if (microcode2 & 32768) orLine = true;
+        busAddressLatch = busAddressRegister;
+        break;
+    case 4:
+        // rising edge
+        if (microcode1 & 2) {
+            busAddressLatch = busAddressRegister;
+            readRequest = true;
+            busOut = true;
         }
-        else {
-            // falling edge
-            if (microcode2 & 1) actionStep(operand, reg); // AS
-            else if (microcode2 & 32 && interrupt) actionStep(operand, reg);  // EI
-            else if (microcode2 & 64 && zeroFlag) actionStep(operand, reg);   // EZ
-            else if (microcode2 & 128 && carryFlag) actionStep(operand, reg); // EC
+        if (microcode1 & 8)    addrOut = true;
+        if (microcode1 & 512)  accOut = true;
+        if (microcode1 & 2048) regOut = true;
+        if (microcode1 & 8192) opLow = true;
+    case 5:
+        // falling edge
+        if (microcode1 & 1)    busIn = true;
+        if (microcode1 & 4)    mathIn = true;
+        if (microcode1 & 16)   accInLow = true;
+        if (microcode1 & 32)   pcIn = true;
+        if (microcode1 & 64)   arLowIn = true;
+        if (microcode1 & 128)  arIn = true;
+        if (microcode1 & 256)  accIn = true;
+        if (microcode1 & 1024) regIn = true;
+        if (microcode1 & 4096) addrHigh = true;
+        break;
+    case 6:
+        // rising edge
+        if (microcode2 & 2) { carryFlag = false; zeroFlag = false; }
+        if (microcode2 & 16) interrupt = false;
+        if (microcode2 & 256) cRollLine = true;
+        if (microcode2 & 512) cInLine = true;
+        if (microcode2 & 1024) rightShift = true;
+        if (microcode2 & 2048) leftShift = true;
+        if (microcode2 & 4096) invBLine = true;
+        if (microcode2 & 8192) invALine = true;
+        if (microcode2 & 16384) fcLine = true;
+        if (microcode2 & 32768) orLine = true;
+    case 7:
+        // falling edge
+        if (microcode2 & 1) actionStep(operand, reg); // AS
+        else if (microcode2 & 32 && interrupt) actionStep(operand, reg);  // EI
+        else if (microcode2 & 64 && zeroFlag) actionStep(operand, reg);   // EZ
+        else if (microcode2 & 128 && carryFlag) actionStep(operand, reg); // EC
 
-            if (microcode2 & 8) {
-                halt = true;
-                std::ostringstream ss;
-                ss << std::endl << " Clock Halted! Accumulator Value: " << accumulator
-                    << " | 0x" << int_to_hex(accumulator, 4)
-                    << " | 0b" << int_to_bin(accumulator, 16);
-                message2 = ss.str();
-            } // CH
-            opLow = false;
-            addrHigh = false;
-            regOut = false;
-            regIn = false;
-            accOut = false;
-            accIn = false;
-            arIn = false;
-            arLowIn = false;
-            pcIn = false;
-            accInLow = false;
-            addrOut = false;
-            mathIn = false;
-            busOut = false;
-            busIn = false;
+        if (microcode2 & 8) {
+            halt = true;
+            std::ostringstream ss;
+            ss << std::endl << " Clock Halted! Accumulator Value: " << accumulator
+                << " | 0x" << int_to_hex(accumulator, 4)
+                << " | 0b" << int_to_bin(accumulator, 16);
+            message2 = ss.str();
+        } // CH
+        opLow = false;
+        addrHigh = false;
+        regOut = false;
+        regIn = false;
+        accOut = false;
+        accIn = false;
+        arIn = false;
+        arLowIn = false;
+        pcIn = false;
+        accInLow = false;
+        addrOut = false;
+        mathIn = false;
+        busOut = false;
+        busIn = false;
 
-            aluEnable = false;
-            carryIn = false;
+        aluEnable = false;
+        carryIn = false;
 
-            orLine = false;
-            fcLine = false;
-            invALine = false;
-            invBLine = false;
-            leftShift = false;
-            rightShift = false;
-            cInLine = false;
-            cRollLine = false;
+        orLine = false;
+        fcLine = false;
+        invALine = false;
+        invBLine = false;
+        leftShift = false;
+        rightShift = false;
+        cInLine = false;
+        cRollLine = false;
 
-            numInstructionsRun++;
-        }
+        numInstructionsRun++;
         break;
     }
 
-    if (!clock) processorState = (processorState + 1) % 4;
+    processorState = (processorState + 1) % 8;
     return;
 }
 
@@ -204,7 +192,7 @@ void Processor::actionStep(unsigned short operand, unsigned char reg) {
         
         busAddressRegister = busAddressRegister + (mathInput & 255);
     }
-    if (pcIn) { programCounter = mathInput; }
+    if (pcIn) programCounter = mathInput;
     if (accInLow) {
         accumulator = (accumulator & 0b1111111100000000) + (mathInput % 256);
     }
